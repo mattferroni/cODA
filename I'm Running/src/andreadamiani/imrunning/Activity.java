0b4509d080cabb3a6a1ed7fb9a0c8a099f653ea6
Andrea Damiani <andreadamiani90@gmail.com>
2014-02-11 15:00:19 +0100

    I'm Running project created and initialized

---
+++ I'm Running/src/andreadamiani/imrunning/Activity.java
@@ -0,0 +1,20 @@
+package andreadamiani.imrunning;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class Activity extends android.app.Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.options, menu);
+        return true;
+    }
+}
